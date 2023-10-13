3f768f156b635570fa175130735fb1806377fb40
Adam Karaki <57075694+AdamKaraki@users.noreply.github.com>
2023-10-12 20:45:11 -0700

    Updated header.h, removed unnecessary tokens

--- header.h
+++ header.h
@@ -1,15 +1,6 @@
 #ifndef SCANTYPE_H
 #define SCANTYPE_H
 
-// Define token classes
-#define BOOLCONST 1
-#define NUMBER 2
-#define IDENTIFIER 3
-#define FLOAT 4
-#define PLUS 5
-#define MINUS 6
-// Define other token classes as needed...
-
 // Define the token data structure
 struct TokenData {
     int tokenclass;   // Token class
