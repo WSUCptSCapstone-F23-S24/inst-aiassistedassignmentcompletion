be46d5317289f7c3b3fb341503a562710df6a3d0
Ayush Kumar <77698706+AyushDotEXE@users.noreply.github.com>
2023-10-25 15:05:38 -0700

    Update scanType.h
    Fixed scanType.h

--- scanType.h
+++ scanType.h
@@ -1,22 +1,12 @@
 #ifndef _SCANTYPE_H_
 #define _SCANTYPE_H_
 
-// Define token enumerators
-enum {
-    NUM = 258,
-    BOOLCONST,
-    CHAR,
-    ID
-};
+typedef struct
+{
+    int tokenClass;
+    int numericVal;
+    char *lexeme;
+    int lineNo;
+} TOKEN;
 
-// Define a struct to hold token data
-struct TokenData {
-    int tokenclass; // Token class
-    int linenum;    // Line number where found
-    char* tokenstr; // String representation of the token
-    char cvalue;    // Character value (for CHAR tokens)
-    int nvalue;     // Numeric value or Boolean value
-    char* svalue;   // String value (e.g., for ID tokens)
-};
-
-#endif
+#endif // _SCANTYPE_H_
