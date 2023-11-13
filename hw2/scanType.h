bd8a3c86ebd7b7f9915bdd081fa6c705a4c4badb
cameronkinney <61388291+cameronkinney@users.noreply.github.com>
2023-11-13 14:41:59 -0800

    scanType.h Update

--- hw2/scanType.h
+++ hw2/scanType.h
@@ -1,12 +1,15 @@
-#ifndef _SCANTYPE_H_
-#define _SCANTYPE_H_
+#ifndef SCANTYPE_H
+#define SCANTYPE_H
 
-typedef struct
+class TokenData
 {
-    int tokenClass;
-    int numericVal;
-    char *lexeme;
-    int lineNo;
-} TOKEN;
+public:
+   int tokenclass;  // token class uses flex/yacc token numbers
+   int linenum;     // line where found
+   char * tokenstr; // what string was actually read
+   int nvalue;      // any numeric value or Boolean value
+   char cvalue;     // any character value
+   char * svalue;   // any string value e.g. an id
+};
 
-#endif // _SCANTYPE_H_
+#endif /* SCANTYPE_H */
