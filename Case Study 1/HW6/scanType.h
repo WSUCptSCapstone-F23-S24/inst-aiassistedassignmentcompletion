086fc88d5f318ad46c2a1c207bdca26ce6b004c7
cameronkinney <61388291+cameronkinney@users.noreply.github.com>
2024-01-23 19:41:14 -0800

    Revert "AI Merge to Case Study 1 Directory"

--- Case Study 1/HW6/scanType.h
+++
@@ -1,6 +0,0 @@
-#ifndef SCANTYPE_H
-#define SCANTYPE_H
-
-struct TokenData {
-    int  tokenClass;            int  lineNum;               char *tokenStr;             char cValue;                int  nValue;                char *sValue;           };
-#endif
